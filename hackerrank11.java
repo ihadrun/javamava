41950146a6749d840804b96771e9f016de15ecde
ihadrun <89574400+ihadrun@users.noreply.github.com>
2022-01-26 23:51:12 +0530

    Add files via upload

---
+++ hackerrank11.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+//get both the values using scanf, and then check if they are lesser than zero , if they are lesser than zero then dsplay the error text or else display the area
+//Write your code here
+static boolean flag = true;
+
+
+static{
+    int flag;
+    Scanner scan =new Scanner(System.in);
+    int B= in.nextInt();
+    int H= in.nextInt();
+  if (B>0 && H)
+  {
+      int area2=B*H;
+      System.out.print(area);
+  }
+  else 
+  {
+      System.out.print("java.lang.Exception: Breadth and height must be positive");}
+    
+}     
+    
+}
+public static void main(String[] args){
