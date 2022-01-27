e6c101267ca15243d5999ef458453ce2c653f878
ihadrun <anirudhoffline@gmail.com>
2022-01-27 15:59:38 +0530

    hackerrank11.java

--- hackerrank11.java
+++ hackerrank11.java
@@ -7,24 +7,28 @@ import java.util.regex.*;
 public class Solution {
 //get both the values using scanf, and then check if they are lesser than zero , if they are lesser than zero then dsplay the error text or else display the area
 //Write your code here
-static boolean flag = true;
-
+    
+static boolean flag = true; 
+static int B,H;
 
 static{
-    int flag;
-    Scanner scan =new Scanner(System.in);
-    int B= in.nextInt();
-    int H= in.nextInt();
-  if (B>0 && H)
-  {
-      int area2=B*H;
-      System.out.print(area);
-  }
-  else 
-  {
-      System.out.print("java.lang.Exception: Breadth and height must be positive");}
-    
-}     
-    
+
+Scanner scan = new Scanner(System.in);
+B = scan.nextInt();
+
+H = scan.nextInt();
+
+if(B>0 && H>0){
+    flag = true;
+
+}else if(B<=0 || H<=0){
+    flag=false;
+    System.out.println("java.lang.Exception: Breadth and height must be positive");
 }
+}
+
+
+    
+    
+
 public static void main(String[] args){
