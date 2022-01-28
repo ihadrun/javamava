59ead78b067fe2d949738470ee85d7d723f8f808
ihadrun <anirudhoffline@gmail.com>
2022-01-28 11:33:36 +0530

    Tutorial 29- Public, private , protected/

---
+++ Tutorial 29- Public, private , protected/src/world/Field.java
@@ -0,0 +1,12 @@
+package world;
+
+public class Field {
+    private Plant plant = new Plant();
+    
+    public Field() {
+        
+        // size is protected; Field is in the same package as Plant.
+        
+    }
+}
+
