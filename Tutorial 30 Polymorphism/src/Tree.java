ea0a121d43598b6aacbe41c7d1f870fcd0842215
ihadrun <anirudhoffline@gmail.com>
2022-01-28 12:34:16 +0530

    Tutorial 30 Polymorphism/

---
+++ Tutorial 30 Polymorphism/src/Tree.java
@@ -0,0 +1,14 @@
+
+public class Tree extends Plant {
+
+	@Override
+	public void grow() {
+System.out.println("tree growing ");
+	}
+	
+public void shedLeaves() {	
+	System.out.println("leaves shedding");
+}
+	
+
+}
