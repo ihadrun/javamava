300fc5cc5b2d1a6c36a3bf28e40f02287b9f9eb4
ihadrun <anirudhoffline@gmail.com>
2022-01-31 17:12:56 +0530

    Tutorial 31. Encapsulation and API Docs/

---
+++ Tutorial 31. Encapsulation and API Docs/src/App.java
@@ -0,0 +1,34 @@
+class Plant{
+	public static float ID =7;
+	private String name;
+
+	public String getData() {
+		return name;
+		
+	}
+	public int calculateGrowthForecast() {
+		return 9;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
+public class App {
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
