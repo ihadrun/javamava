35fb2265af26d71f692040dac1616029c2d1b12a
ihadrun <anirudhoffline@gmail.com>
2022-02-01 14:26:39 +0530

    32.Casting numerical values/

---
+++ 32.Casting numerical values/src/App.java
@@ -0,0 +1,30 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int intValue = 888;
+		short shortValue = 44;
+		byte byteValue = 20;
+		long longValue = 334522;
+		float floatValue2 =(float)99.3;
+		float floatValue = 4483.3f;
+		double doubleValue = 32.4;
+		
+		System.out.println(Byte.MAX_VALUE);
+		
+
+		intValue = (int)longValue;
+		
+		System.out.println(intValue);
+		// TODO Auto-generated method stub
+doubleValue = intValue;
+System.out.println(doubleValue);
+
+intValue = (int)floatValue;
+System.out.println(intValue);
+
+byteValue  =(byte)128;
+	System.out.println(byteValue);}
+
+}
