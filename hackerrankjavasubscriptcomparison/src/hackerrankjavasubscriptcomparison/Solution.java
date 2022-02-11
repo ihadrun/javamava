7e9876c5bcdda1eeabbc7da806480f84bd46744c
ihadrun <89574400+ihadrun@users.noreply.github.com>
2022-02-11 11:21:30 +0530

    Add files via upload

---
+++ hackerrankjavasubscriptcomparison/src/hackerrankjavasubscriptcomparison/Solution.java
@@ -0,0 +1,37 @@
+package hackerrankjavasubscriptcomparison;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+    	String curr = s.substring(0,k);
+        String smallest =curr;
+        String largest = curr;
+        for(int i=k;i<s.length();i++)
+        { curr = curr.substring(k,i) + s.charAt(i);
+        if (smallest.compareTo(curr) < 0) {
+        	smallest =curr;
+        }
+        if (largest.compareTo(curr) > 0) {
+        	largest =curr;
+        }
+        
+        
+        }
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
