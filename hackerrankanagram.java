8bb1647741652d97de01063982ef89c2aeebcbca
ihadrun <89574400+ihadrun@users.noreply.github.com>
2022-02-16 15:55:25 +0530

    Create hackerrankanagram.java

---
+++ hackerrankanagram.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+static boolean isAnagramSort(String string1, String string2) {
+    if (string1.length() != string2.length()) {
+        return false;
+    }
+    char[] a1 = string1.toCharArray();
+    char[] a2 = string2.toCharArray();
+    Arrays.sort(a1);
+    Arrays.sort(a2);
+    return Arrays.equals(a1, a2);
+}
+    
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagramSort(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
